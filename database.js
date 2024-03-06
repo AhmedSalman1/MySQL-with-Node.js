cc0885e2a05d3fea09adb1c05e2e759b33c0a2c3
AhmedSalman <medosalman375@gmial.com>
2024-03-06 10:06:12 +0200

    Add Notes

--- database.js
+++ database.js
@@ -11,10 +11,39 @@ const pool = mysql
 	})
 	.promise();
 
-async function getNotes() {
+export async function getNotes() {
 	const [rows] = await pool.query("SELECT * FROM notes");
 	return rows;
 }
 
-const notes = await getNotes();
-console.log(notes);
+export async function getNote(id) {
+	const [rows] = await pool.query(
+		`
+    SELECT * 
+    FROM notes
+    WHERE id = ? `,
+		[id]
+	);
+	return rows[0];
+}
+
+// const notes = await getNotes();
+// console.log(notes);
+
+// const note = await getNote(1);
+// console.log(note);
+
+export async function createNote(title, contents) {
+	const [result] = await pool.query(
+		`
+        INSERT INTO notes (title, contents) 
+        Values (?, ?)
+    `,
+		[title, contents]
+	);
+	const id = result.insertId;
+	return getNote(id);
+}
+
+// const result = await createNote("test", "create");
+// console.log(result);
